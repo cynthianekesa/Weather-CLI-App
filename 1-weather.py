b03617584a933cc79e546c7c4dd8d74cf0550072
Nekesa <c.nekesa@alustudent.com>
2023-08-18 19:06:21 +0200

    changes tp the python code

---
+++ 1-weather.py
@@ -0,0 +1,14 @@
+import file
+import start
+import weather
+
+ussd = input("Enter USSD ")
+
+if (ussd == '*171#'):
+    file.main_file()
+elif (ussd == '*172#'):
+    start.start()
+elif (ussd == '*173#'):
+    weather.viewWeather()
+else: 
+    print("Invalid Code!")
